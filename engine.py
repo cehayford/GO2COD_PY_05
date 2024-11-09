e5f5b51d3953c80f2b46e1495f85681a61f204b9
cehayford <sean.alpha18@gmail.com>
2024-11-09 00:38:11 +0000

    comment of the hangman class

--- engine.py
+++ engine.py
@@ -2,3 +2,18 @@ import random
 from config import *
 import PySimpleGUI as gui
 
+class HangmanGame:
+    def __init__(self, config: GameConfiguration = None):
+        self.config =  GameConfiguration()
+        self.artist = HangmanArtist()
+        self.validator = GuessValidator()
+        self.reset_game()
+
+    def reset_game(self) -> None:
+        self.word = random.choice(self.config.word_list).lower()
+        self.guessed_letters: Set[str] = set()
+        self.remaining_attempts = self.config.max_attempts
+        self.status = GameStatus.IN_PROGRESS
+        self.display = WordDisplay(self.word, self.guessed_letters)
+
+    
